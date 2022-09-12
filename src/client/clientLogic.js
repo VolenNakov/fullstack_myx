92695f1d370526035e9f5782b224bec71026142e
Volen Nakov <volen.nakov@gmail.com>
2022-09-13 02:07:36 +0300

    Added modal view for the original size of the img

--- src/client/clientLogic.js
+++ src/client/clientLogic.js
@@ -1,7 +1,12 @@
-function getImages(pageNumber) {
+let currentPage = 1;
+const skeletCount = 5;
+let pageSize = 30;
+let container, loader, modal, modalImg, closeBtn;
+
+function getImages(pageNumber, pageSize) {
   const fetchData = {
     method: "POST",
-    body: JSON.stringify({ pageNumber: pageNumber }),
+    body: JSON.stringify({ pageNumber: pageNumber, pageSize: pageSize }),
     headers: new Headers({
       "Content-Type": "application/json; charset=UTF-8",
     }),
@@ -9,23 +14,84 @@ function getImages(pageNumber) {
 
   fetch("http://localhost:3000/images", fetchData)
     .then((res) => res.json())
-    .then((data) =>
-      data.data.forEach((element) => {
-        let img = new Image();
-        img.src = `http://localhost:3000/uploads/${element}`;
-        document.body.appendChild(img);
-      })
-    )
-}
-getImages(1);
-let currentPage = 1;
-const handleInfiniteScroll = () => {
-  const endOfPage =
-    window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+    .then((resData) =>
+      resData.data.forEach((element) => {
+        const card = document.createElement("div");
+        card.className = "card";
 
-  if (endOfPage) {
-    getImages(currentPage + 1);
-    currentPage++;
-  }
+        const img = new Image();
+        img.src = `http://localhost:3000/thumbnails/${element}`;
+        img.onclick = () => {
+          modal.style.display = "block";
+          modalImg.src = `http://localhost:3000/uploads/${
+            element.split(".webp")[0]
+          }`;
+        };
+
+        const zoomIcon = document.createElement("span");
+        zoomIcon.className = "material-symbols-outlined";
+        zoomIcon.innerHTML = "zoom_out_map";
+
+        card.appendChild(zoomIcon);
+        card.appendChild(img);
+        container.appendChild(card);
+      })
+    );
+}
+
+let throttleTimer;
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+
+  throttleTimer = true;
+
+  setTimeout(() => {
+    callback();
+    throttleTimer = false;
+  }, time);
 };
+
+const handleInfiniteScroll = () => {
+  throttle(() => {
+    const endOfPage =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+
+    if (endOfPage) {
+      getImages(currentPage + 1, pageSize);
+    }
+
+    // if (currentPage === pageCount) {
+    //   removeInfiniteScroll();
+    // }
+  }, 1000);
+};
+
 window.addEventListener("scroll", handleInfiniteScroll);
+
+document.addEventListener("DOMContentLoaded", () => {
+  loader = document.querySelector("#loader");
+  container = document.querySelector("#container");
+  modal = document.querySelector("#modal");
+  modalImg = document.querySelector("#modalImg");
+  closeBtn = document.querySelector(".close");
+
+  closeBtn.onclick = () => {
+    modal.style.display = "none";
+  };
+
+  const cardSkeleton = document.createElement("div");
+  cardSkeleton.className = "card-skeleton";
+  for (let i = 0; i < skeletCount; i++) {
+    loader.appendChild(cardSkeleton.cloneNode(true));
+  }
+});
+
+window.onload = () => {
+  getImages(currentPage, pageSize);
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const gm = require('gm');
-
-const app = express();
-
+const sharp = require("sharp");
 const fs = require("fs");
 
 const port = 3000;
 const imageFolder = "/../images/";
+const thumbnailFolder = "/../images/thumbnails/";
 
+const app = express();
+//App Middlewares
 app.use(bodyParser.json());
-// TODO Трябва да сменя express.static с res.sendFile(path.join(public, 'index.html'));
-app.use("/uploads", express.static(path.join(__dirname + "/../images"),{maxAge:3600000}));
+
+//App Static files
+app.use(
+  "/thumbnails",
+  express.static(path.join(__dirname + thumbnailFolder), {
+    maxAge: 3600000,
+  })
+);
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname + imageFolder), {
+    maxAge: 3600000,
+  })
+);
 app.use("/static", express.static(path.join(__dirname + "/client")));
 
 function paging(inputArr, currentPage, pageSize) {
@@ -33,12 +45,15 @@ function paging(inputArr, currentPage, pageSize) {
   };
 }
 
-app.post("/images/", async (req, res) => {
+//App routes
+
+app.post("/images/", (req, res) => {
+  const { pageNumber, pageSize } = req.body;
   const images = [];
-  fs.readdirSync(path.join(__dirname + imageFolder)).forEach((file) =>
-    images.push(file)
+  fs.readdirSync(path.join(__dirname + thumbnailFolder)).forEach((fileName) =>
+    images.push(fileName)
   );
-  res.send(paging(images, req.body.pageNumber, 25));
+  res.send(paging(images, pageNumber, pageSize));
 });
 
 app.listen(port, () => {
