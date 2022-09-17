a332754b35843a7c9ee3f606a68f449f25e8a8f6
Volen Nakov <volen.nakov@gmail.com>
2022-09-18 01:44:44 +0300

    FINAL COMMIT :D

--- src/app.js
+++ src/app.js
@@ -1,36 +1,23 @@
-const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
+const multer = require("multer");
 const express = require("express");
 const sqlite3 = require("sqlite3");
 
-const db = new sqlite3.Database(__dirname+"/images.db",sqlite3.OPEN_READWRITE);
+const db = new sqlite3.Database(
+  __dirname + "/images.db",
+  sqlite3.OPEN_READWRITE
+);
 
 const port = 3000;
-const imageFolder = "/uploads/";
-const thumbnailFolder = "/thumbnails/";
+const imageFolder = path.join(__dirname + "/uploads/");
+const thumbnailFolder = path.join(__dirname + "/thumbnails/");
+
+const upload = multer({ dest: imageFolder });
 
 const app = express();
 //App Middlewares
 app.use(express.json());
-
-function paging(inputArr, currentPage, pageSize) {
-  const trimStart = (currentPage - 1) * pageSize;
-  const trimEnd = trimStart + pageSize;
-  const itemsQty = inputArr.length;
-
-  const pageCount = Math.ceil(itemsQty / pageSize);
-
-  return {
-    images: inputArr.slice(trimStart, trimEnd),
-    paging: {
-      currentPage: currentPage,
-      pageSize: pageSize,
-      pageCount: pageCount,
-      total: itemsQty,
-    },
-  };
-}
 
 //App routes
 
@@ -38,31 +25,54 @@ app.post("/images/", (req, res) => {
   const { pageNumber, pageSize } = req.body;
   const images = []; // Array for the image names
   try {
-    fs.readdirSync(path.join(__dirname + thumbnailFolder)).forEach((fileName) =>
-      images.push(fileName)
-    );
-    res.status(200).send(paging(images, pageNumber, pageSize));
+    db.get(`SELECT COUNT(id) FROM images`, (err, row) => {
+      const total = row["COUNT(id)"];
+      db.all(
+        `SELECT * FROM images ORDER BY uploaded DESC LIMIT ${pageSize} OFFSET ${
+          (pageNumber - 1) * pageSize
+        }`,
+        (err, rows) => {
+          if (err) {
+            return res.status(500).send(JSON.stringify(err));
+          }
+          rows.forEach((row) => {
+            images.push(row.name);
+          });
+          res.status(200).send({
+            images,
+            paging: {
+              currentPage: pageNumber,
+              pageSize: pageSize,
+              pageCount: Math.ceil(total / pageSize),
+              total: total,
+            },
+          });
+        }
+      );
+    });
   } catch (err) {
     res.status(500).send(`There was problem with your request: ${err}`);
   }
 });
 
-app.post("/upload/", (req, res) => {
-  const { fileName } = req.body;
-  console.log(req.body)
+app.post("/upload/", upload.array("image"), async (req, res) => {
+  const { files } = req;
+  const file = files[0];
+  await sharp(file.path)
+    .resize(256, 256)
+    .toFormat("webp")
+    .toFile(thumbnailFolder + file.filename + ".webp");
+
   db.run(
-    "INSERT INTO images(name, created) VALUES(?, ?)",
-    [fileName, Date.now()],
+    "INSERT INTO images(name, originalname, uploaded) VALUES (?,?,?)",
+    [file.filename, file.originalname, Date.now()],
     (err) => {
       if (err) {
-        console.log(err)
-        return res.status(500).send(err)
+        return res.status(500).send(err);
       }
-      res.status(200).send("uraaa")
-      console.log("Row was added to the table: ${this.lastID}");
+      res.status(200).send({ imageName: file.filename });
     }
   );
-
 });
 
 app.listen(port, () => {
@@ -73,13 +83,13 @@ app.listen(port, () => {
 
 app.use(
   "/thumbnails",
-  express.static(path.join(__dirname + thumbnailFolder), {
+  express.static(thumbnailFolder, {
     maxAge: 86400000, //1 day cache policy
   })
 );
 app.use(
   "/uploads",
-  express.static(path.join(__dirname + imageFolder), {
+  express.static(imageFolder, {
     maxAge: 86400000, //1 day cache policy
   })
 );
--- src/public/clientLogic.js
+++ src/public/clientLogic.js
@@ -1,3 +1,7 @@
+let currentPage = 1,
+  pageCount,
+  pageSize;
+
 const container = document.querySelector("#container");
 const modal = document.querySelector("#modal");
 const modalImg = document.querySelector("#modalImg");
@@ -8,9 +12,67 @@ closeBtn.onclick = () => {
   modal.style.display = "none";
 };
 
-let currentPage = 1;
-let pageCount;
-let pageSize;
+const favorite = document.querySelector(".star");
+favorite.onclick = () => {
+  const _favorites = parseLocalStorage("favorites");
+  const imageName = modalImg.src.split("uploads/")[1];
+  _favorites.includes(imageName)
+    ? (_favorites.splice(_favorites.indexOf(imageName), 1),
+      document.getElementById(imageName).classList.remove("favorite"))
+    : (_favorites.push(imageName),
+      document.getElementById(imageName).classList.add("favorite"));
+
+  localStorage.setItem("favorites", JSON.stringify(_favorites));
+};
+
+const favorites = document.querySelector("#favorites");
+let favoritesToggle = true;
+favorites.onclick = () => {
+  if (favoritesToggle) {
+    container.innerHTML = "";
+    const _favorites = parseLocalStorage("favorites");
+    _favorites.forEach((image) => {
+      createCard(image);
+    });
+  } else {
+    container.innerHTML = "";
+    currentPage = 0;
+    getImages(1, 100);
+  }
+  favoritesToggle = !favoritesToggle;
+};
+
+const parseLocalStorage = (key) => {
+  const data = localStorage.getItem(key);
+  return JSON.parse(data);
+};
+
+const createCard = (imageName, first) => {
+  const card = cardTemplate.content.cloneNode(true);
+  const img = card.querySelector("img");
+  img.id = imageName;
+  const favorites = parseLocalStorage("favorites");
+
+  if (favorites.includes(imageName)) {
+    img.classList.add("favorite");
+  }
+
+  img.src = `http://localhost:3000/thumbnails/${imageName}.webp`;
+  img.classList.add("skeleton");
+  img.onclick = () => {
+    modal.style.display = "block";
+    modalImg.style.display = "none";
+    modalImg.onload = () => {
+      modalImg.style.display = "block";
+    };
+    modalImg.src = `http://localhost:3000/uploads/${imageName}`;
+  };
+  img.onload = () => {
+    img.classList.remove("skeleton");
+  };
+
+  first ? container.append(card) : container.prepend(card);
+};
 
 const getImages = async (pageNumber, pageSize) => {
   const fetchData = {
@@ -24,27 +86,52 @@ const getImages = async (pageNumber, pageSize) => {
   const response = await fetch("http://localhost:3000/images", fetchData);
   const data = await response.json();
 
-  pageCount = data.paging.pageCount;
-  currentPage = data.paging.currentPage;
+  if (data.paging.total != 0) {
+    pageCount = data.paging.pageCount;
+    currentPage = data.paging.currentPage;
 
-  data.images.forEach((imageName) => {
-    const card = cardTemplate.content.cloneNode(true);
-    const img = card.querySelector("img");
+    data.images.forEach((imageName) => {
+      createCard(imageName, true);
+    });
+  }
+};
 
-    img.src = `http://localhost:3000/thumbnails/${imageName}`;
-    img.classList.add("skeleton");
-    img.onclick = () => {
-      modal.style.display = "block";
-      modalImg.src = `http://localhost:3000/uploads/${
-        imageName.split(".webp")[0]
-      }`;
+const uploadImage = (file) => {
+  return new Promise((resolve, reject) => {
+    const formData = new FormData();
+    formData.append("image", file, file.name);
+
+    const fetchData = {
+      method: "POST",
+      body: formData,
     };
-    img.onload = () => {
-      img.classList.remove("skeleton");
-    };
-
-    container.appendChild(card);
+    setTimeout(() => {
+      fetch("http://localhost:3000/upload", fetchData)
+        .then((res) => res.json())
+        .then((data) => createCard(data.imageName), resolve());
+    }, 3000);
   });
+};
+
+const uploadImages = async () => {
+  const files = [...document.querySelector("#upload").files];
+  files.sort((a, b) => b.lastModified - a.lastModified);
+  const batches = [];
+  const batchesSize = 25;
+
+  for (let i = 0; i < files.length; i += batchesSize) {
+    batches.push(files.slice(i, i + batchesSize));
+  }
+
+  for (let i = 0; i < batches.length; i++) {
+    const promises = [];
+
+    for (let j = 0; j < batches[i].length; j++) {
+      promises.push(uploadImage(batches[i][j]));
+    }
+    await Promise.all(promises);
+  }
+  currentPage += files.length / pageSize;
 };
 
 let throttleTimer;
@@ -57,10 +144,6 @@ const throttle = (callback, time) => {
     callback();
     throttleTimer = false;
   }, time);
-};
-
-const removeInfiniteScroll = () => {
-  window.removeEventListener("scroll", handleInfiniteScroll);
 };
 
 const handleInfiniteScroll = () => {
@@ -78,7 +161,7 @@ const handleInfiniteScroll = () => {
     }
 
     if (currentPage === pageCount) {
-      removeInfiniteScroll();
+      window.removeEventListener("scroll", handleInfiniteScroll);
     }
   }, 500);
 };
@@ -86,6 +169,9 @@ const handleInfiniteScroll = () => {
 window.addEventListener("scroll", handleInfiniteScroll);
 
 window.onload = async () => {
+  if (!parseLocalStorage("favorites")) {
+    localStorage.setItem("favorites", "[]");
+  }
   cardsPerRow = Math.floor(container.offsetWidth / 255);
   rowsPerWindow = Math.floor(window.innerHeight / 255) + 1; //The + 1 is to make sure that the window has scroll bar
 
